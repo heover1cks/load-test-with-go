7bfd81e59a45c73fcb1bfbeb215cb94531c69ce8
heover1cks <heover1cks@gmail.com>
2020-06-25 21:51:21 +0900

    Simple Load Tester with Go

---
+++ loadTest.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+"fmt"
+"github.com/codeskyblue/go-sh"
+"strconv"
+"time"
+)
+
+var mainUri = "LBADDRESS"
+var subPath = "/tct/user/"
+var patchStr = "?job=job&name=name&phone=phone"
+
+func curlExec(id int) {
+	path := mainUri + subPath + strconv.Itoa(id) + patchStr
+	fmt.Println(path)
+	for {
+		sh.Command("curl", "-X", "PATCH", path, "-H", "accept: */*").Run()
+		time.Sleep(time.Millisecond * 100)
+		fmt.Println("ID:" + strconv.Itoa(id) + " PATCHed")
+	}
+}
+
+func main() {
+	for i := 1; i < 2000; i++ {
+		time.Sleep(time.Millisecond * 1)
+		go curlExec(i)
+	}
+}
